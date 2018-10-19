81e15f45db7350ebf78e1ca78f2fe3fd6d9f0d3d
Jack Casey <jack@Jacks-MBP.hitronhub.home>
2018-10-18 22:51:56 -0700

    Re-wrote program completely

--- start.py
+++ start.py
@@ -1,31 +1,36 @@
-def GUIDtoPID(GUID):
-
-    # Remove characters we do not want
-
+def cleanGUID(input): #Removes accessory characters from GUID
     removedChar = '{}- '
     for char in removedChar:  
-        GUID = GUID.replace(char,"")
+        input = input.replace(char,"")
+    return input
 
-    # Ensure there are 32 characters in the string
+def addbrackets(input): #Adds accessory characters to PID
+    return '{{{0}-{1}-{2}-{3}-{4}}}'.format(input[:8], input[8:12], input[12:16], input[16:20], input[20:])
 
-    if len(GUID) == 32:
-        print ("GUID Valid: Converting...")
-    else:
-        print ("GUID Invalid. Please try again.")
-        return
-        
-    # Arrange the characters in the reversed format
+def isvalid(input): #Checks if length of input is 32 characters
+    return len(input) == 32
 
+def flip(input): #Flips order of input
     order = [7,6,5,4,3,2,1,0,11,10,9,8,15,14,13,12,17,16,19,18,21,20,23,22,25,24,27,26,29,28,31,30]
-    PID = ""    
+    output = ""    
     for i in order:
-        PID = PID + GUID[i]
-    print(PID)
-    return PID
+        output = output + input[i]
+    return output
+   
+def flipguid(input): #Checks if input is GUID or PID, and converts accordingly
+    if input[0] == '{' and isvalid(cleanGUID(input)): #Returns true if input is a valid GUID
+        return flip(cleanGUID(input))
 
-GUIDtoPID(input("Enter GUID: "))
+    if isvalid(input): #Returns true if input is a valid PID
+        return (addbrackets(flip(input)))
+
+    else: #If the output is invalid
+        return ("Invalid input, please try again.")
     
-    
+
+
+
+
     
     
     
